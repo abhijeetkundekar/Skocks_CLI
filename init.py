d99b6b882ddf0cd527524733f9e882dbbee25223
Abhijeet <abhi@botzee.com>
2025-05-09 19:36:29 +0530

    added polygon for stock api access

---
+++ init.py
@@ -0,0 +1,34 @@
+import argparse
+import sys
+from polygon import RESTClient
+import json
+
+#using sys
+# n = len(sys.argv)
+# print(f"Number of arguments: {n}")
+# print(f"Argument List: {sys.argv}")
+# [progam, x, y] = sys.argv
+# print("total is ", int(x) + int(y))
+
+#using argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("name", type=str, help="ticker name")
+
+args = parser.parse_args()
+
+
+
+
+# docs
+# https://polygon.io/docs/stocks/get_v3_reference_tickers__ticker
+# https://polygon-api-client.readthedocs.io/en/latest/Reference.html#get-ticker-details
+
+client = RESTClient("j0ipMPp_4w_LHhIFGJxd0Un8s_AE2FLQ") # hardcoded api_key is used
+#client = RESTClient()  # POLYGON_API_KEY environment variable is used
+
+stock = client.get_daily_open_close_agg(args.name, "2025-02-07")
+
+print(type(stock))
+print(stock.close)
